9a501a1430d9233cf8a39add48048df9af2f5a2b
Pushpahas Kuchipudi <pushpak0823@gmail.com>
2024-09-13 15:15:37 -0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Pushpahas Kuchipudi on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
